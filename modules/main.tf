0bb4a9a46c439de9c715f605ec19c458460d5e41
judahtc <jaycea247@gmail.com>
2025-06-17 13:00:32 +0200

    feat: filled in the module

---
+++ modules/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "s3_website" {
+  bucket = var.bucket_name
+  tags = {
+    env     = var.env
+    app     = var.app
+    version = var.version
+    stack   = var.version
+  }
+
+
+
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.s3_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.s3_website.arn}/*"
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_s3_bucket_website_configuration" "s3_website_config" {
+  bucket = aws_s3_bucket.s3_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+
+}
+
