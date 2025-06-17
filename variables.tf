324c893bf23732d58a32a7c547fbc5ba7150f391
judahtc <jaycea247@gmail.com>
2025-06-17 14:39:54 +0200

    feat: implemented optional variables

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "git_branch" {
+  description = "Git branch to be used"
+  type        = string
+}
+
+variable "full_repository_id" {
+  description = "Full GitHub repository ID (e.g., org/repo)"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "app" {
+  description = "Application name"
+  type        = string
+  default     = ""
+}
+
+variable "version" {
+  description = "Version number"
+  type        = string
+  default     = ""
+}
