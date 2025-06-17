8fde302f6771fd7558c1ade83522511ce80ed860
judahtc <jaycea247@gmail.com>
2025-06-17 14:43:27 +0200

    feat: called module for the first time

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "s3_cf_route53" {
   source = "./modules"
 
-  full_repository_id = ""
-  aws_region = ""
-  git_branch = ""
-  bucket_name = ""
-  env = ""
+  full_repository_id = var.full_repository_id
+  aws_region = var.aws_region
+  git_branch = var.git_branch
+  bucket_name = var.bucket_name
+  env = var.env
   
+
 }
