54571c1d62ecbb4dc64f1421f9e9ea22d0e3822a
judahtc <jaycea247@gmail.com>
2025-06-18 17:55:39 +0200

    feat: added error pages

--- modules/main.tf
+++ modules/main.tf
@@ -389,4 +389,25 @@ origin_access_control_id = var.origin_access_identity
 
   price_class  = "PriceClass_All"
   http_version = "http2"
+
+
+    custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+    tags = {
+    env = var.env
+    app     = var.app
+    version = var.version_number
+  }
 }
