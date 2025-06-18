50a64a028a502c9458cf8bebf2ce6224a6d70620
judahtc <jaycea247@gmail.com>
2025-06-18 09:43:47 +0200

    feat: created a cloudfront resource

--- modules/main.tf
+++ modules/main.tf
@@ -303,3 +303,72 @@ resource "aws_codepipeline" "frontend_codepipeline" {
   }
 }
 
+resource "aws_cloudfront_distribution" "claxon_frontend" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = var.cloudfront_description
+  default_root_object = "index.html"
+
+  aliases = [var.alternate_domain]
+
+  origin {
+    domain_name = aws_s3_bucket.s3_website.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+
+    s3_origin_config {
+      origin_access_identity = var.origin_access_identity
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = [
+      "GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"
+    ]
+
+    cached_methods = [
+      "GET", "HEAD"
+    ]
+
+    compress = true
+
+    forwarded_values {
+      query_string = false
+
+      headers = [
+        "Authorization",
+        "Origin",
+        "Accept",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers"
+      ]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 0
+    max_ttl     = 0
+
+    response_headers_policy_id = "60669652-455b-4ae9-85a4-c4c02393f86c"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class  = "PriceClass_All"
+  http_version = "http2"
+}
